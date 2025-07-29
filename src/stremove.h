c5c23bb46ef56f94fc9197cc028852444e3ae091
Juha A Sinisalo <162747980+jsinisal@users.noreply.github.com>
2025-07-29 13:53:34 +0300

    Add files via upload
    New function. Removes or Keeps given set of characters in a string or a list. Default is remove. By specifying optional keyword parameter action="keep" only given characters are kept.

---
+++ src/stremove.h
@@ -0,0 +1,12 @@
+//
+// Created by sinj on 29/07/2025.
+//
+
+#ifndef STREMOVE_H
+#define STREMOVE_H
+
+#include <Python.h>
+
+PyObject* stremove(PyObject *self, PyObject *args, PyObject *kwargs);
+
+#endif //STREMOVE_H
